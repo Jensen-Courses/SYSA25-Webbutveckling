68fbf438980452c2ce91fa96251afe744e896478
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-05-05 15:12:45 +0200

    End of lesson 2

--- Lektion-02/RunStore/Program.cs
+++ Lektion-02/RunStore/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RunStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EShopContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -13,5 +21,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+await CreateDatabase.InitDb(app);
 
 app.Run();
