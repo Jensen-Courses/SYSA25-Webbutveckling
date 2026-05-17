dcd31164b11a6cee225529e3eabb87ef9004282f
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-05-17 17:29:04 +0200

    Added starter for lesson 4

---
+++ Lektion-04/RunStoreApi/api/Helpers/SingleResult.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Helpers;
+
+public class SingleResult<R>(R data)
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 1;
+    public int Items { get; set; } = 1;
+    public R Data { get; set; } = data;
+}
