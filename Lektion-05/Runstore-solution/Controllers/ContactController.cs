3d9e0ac24a7ea98287aa670ea282875f078da619
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-05-19 11:20:26 +0200

    Updated solution

--- Lektion-05/Runstore-solution/Controllers/ContactController.cs
+++ Lektion-05/Runstore-solution/Controllers/ContactController.cs
@@ -5,7 +5,14 @@ namespace RunStore.Controllers
     public class ContactController : Controller
     {
         // GET: ContactController
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add()
         {
             return View();
         }
