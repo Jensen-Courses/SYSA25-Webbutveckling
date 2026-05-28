b50a0da4d46449905bdef6cdda020c3e8110794a
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-05-28 12:06:27 +0200

    Added solution for lesson 8

---
+++ Lektion-08/runStore-solution/src/pages/products/products.js
@@ -0,0 +1,28 @@
+const productList = document.querySelector('#product-list');
+
+const initApp = () => {
+  console.log('App is loading...');
+  displayProducts();
+};
+
+const displayProducts = () => {
+  console.log(products);
+  let html = '';
+
+  products.map(
+    (product) =>
+      (html += `
+        <section class="product-item">
+            <h4>${product.name}</h4>
+            <a href="product-details.html?id=${product.id}">
+                <img src="../../assets${product.imageUrl}" alt="${product.name}"/>
+            </a>
+            <p>${product.price.toFixed(2)} Kr</p>
+        </section>
+    `),
+  );
+
+  productList.innerHTML = html;
+};
+
+initApp();
